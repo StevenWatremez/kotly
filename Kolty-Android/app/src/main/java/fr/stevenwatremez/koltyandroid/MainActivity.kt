6ef085826e37dfd8e6ca308154a1d04434e0a764
Steven Watremez <steven.watremez@niji.fr>
2017-06-26 09:34:32 +0200

    add basic views

--- Kolty-Android/app/src/main/java/fr/stevenwatremez/koltyandroid/MainActivity.kt
+++ Kolty-Android/app/src/main/java/fr/stevenwatremez/koltyandroid/MainActivity.kt
@@ -2,12 +2,13 @@ package fr.stevenwatremez.koltyandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-
+    firstTextView.text = "coucou"
   }
 }
--- Kotly-iOS/Kotly iOS/View/ViewController.swift
+++ Kotly-iOS/Kotly iOS/View/ViewController.swift
@@ -9,17 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet private var firstTextView: UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    self.setupSubviews()
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  private func setupSubviews() {
+    self.firstTextView.text = "coucou"
   }
-
-
 }
-
